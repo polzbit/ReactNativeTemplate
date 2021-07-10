f5a28815fe24d007f2d37a27881e09e57aa043f3
polzbit <38733198+polzbit@users.noreply.github.com>
2021-07-10 23:32:53 +0300

    adding preloader feature

--- NativeTemplate/App.js
+++ NativeTemplate/App.js
@@ -1,14 +1,37 @@
 
-import React from 'react';
+import React, {useState, useEffect}from 'react';
+import { View, StyleSheet } from "react-native";
 import AppDrawer from "./src/navigator/AppDrawer";
+import PreLoader from './src/components/Preloader';
 
 const App = () => {
+  const [state, setState] = useState({
+		loading: true
+	}); 
+
+  useEffect(() => {
+		setTimeout(() => {
+			setState(prevState => ({
+                ...prevState, 
+                loading: false,}));
+			}, 4000)
+	});
+  
   return (
-    
-    <AppDrawer />
-    
+    <View style={styles.App}>
+      {state.loading ?
+        <PreLoader preLoaderVisible={state.loading} />
+      :
+      <AppDrawer />
+      }
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  App: {
+    flex: 1,
+  }
+});
 
 export default App;
